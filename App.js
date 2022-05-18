df50738ab65e91425267c59731520e192ae36a25
kishor770 <56428071+kishor770@users.noreply.github.com>
2022-05-18 12:56:42 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-// import Login from './components/Login';
-// import Signup from './components/Signup';
+ import Login from './components/Login';
+ import Signup from './components/Signup';
 import Home from './components/Home.js';
 
 function App() {
@@ -8,8 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<Home />} />
-        {/* <Route path='/signup' element={<Signup/>} />
-        <Route path='/login' element={<Login/>} /> */}
+         <Route path='/signup' element={<Signup/>} />
+        <Route path='/login' element={<Login/>} /> 
       </Routes>
     </BrowserRouter>
   );
